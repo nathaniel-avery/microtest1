a0c3867a92612a07a9d77bd630d6c486c9e23ae1
Nathaniel Avery <nathaniel.avery@gmail.com>
2022-02-22 15:43:48 +0000

    expanded api for fake movies db

--- app/main.py
+++ app/main.py
@@ -1,9 +1,26 @@
 #~/movie_service/app/main.py
 
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import List
 
 app = FastAPI()
 
-@app.get('/')
+fake_movie_db = [
+    {
+        'name': 'Star Wars: Episode IX - The Rise of Skywalker',
+        'plot': 'The surviving members of the resistance face the First Order once again.',
+        'genres': ['Action', 'Adventure', 'Fantasy'],
+        'casts': ['Daisy Ridley', 'Adam Driver']
+    }
+]
+
+class Movie(BaseModel):
+    name: str
+    plot: str
+    genres: List[str]
+    casts: List[str]
+
+@app.get('/', response_model=List[Movie])
 async def index():
-    return {"Real": "Python"}
+    return fake_movie_db
