4f61ce0df0769f56a8778d0bb3ac1eb3ab69c26d
Nathaniel Avery <nathaniel.avery@gmail.com>
2022-02-22 15:49:52 +0000

    added post

--- app/main.py
+++ app/main.py
@@ -24,3 +24,10 @@ class Movie(BaseModel):
 @app.get('/', response_model=List[Movie])
 async def index():
     return fake_movie_db
+
+@app.post('/', status_code=201)
+async def add_movie(payload: Movie):
+    movie = payload.dict()
+    fake_movie_db.append(movie)
+    return {'id': len(fake_movie_db) - 1}
+
