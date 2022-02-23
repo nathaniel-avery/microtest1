5e6220e870c798487b11db2a48679a2996fff0ba
Nathaniel Avery <nathaniel.avery@gmail.com>
2022-02-23 03:53:11 +0000

    added to the api

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from typing import List
+from fastapi import HTTPException
 
 app = FastAPI()
 
@@ -30,4 +31,23 @@ async def add_movie(payload: Movie):
     movie = payload.dict()
     fake_movie_db.append(movie)
     return {'id': len(fake_movie_db) - 1}
+
+@app.put('/{id}')
+async def update_movie(id: int, payload: Movie):
+    movie = payload.dict()
+    movies_length = len(fake_movie_db)
+    if 0 <= id <= movies_length:
+        fake_movie_db[id] = movie
+        return None
+    raise HTTPException(status_code=404, detail="Movie with given id not found")
+
+@app.delete('/{id}')
+async def delete_movie(id: int):
+    movies_length = len(fake_movie_db)
+    if 0 <= id <= movies_length:
+        del fake_movie_db[id]
+        return None
+    raise HTTPException(status_code=404, detail="Movie with given id not found")
+
+
 
